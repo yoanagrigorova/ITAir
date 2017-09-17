77939e75a551557b97bd7a95044ba32f62dd69b2
yoanagrigorova <31156123+yoanagrigorova@users.noreply.github.com>
2017-09-17 22:44:09 +0300

    better nav

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,13 @@
+var planButton = document.querySelector("nav ul li button");
+
 function showMenu(isOn) {
     if (isOn) {
         document.querySelector("header~div").style.display = "inline-block";
+        planButton.style.color = "navy";
+        planButton.style.backgroundColor = "white";
     } else {
         document.querySelector("header~div").style.display = "none";
+        planButton.style.color = "white";
+        planButton.style.backgroundColor = "navy";
     }
 }
