2690acf0466007264779dd8163bc05170732e66e
yoanagrigorova <31156123+yoanagrigorova@users.noreply.github.com>
2017-09-22 16:33:11 +0300

    user sign in and functionalities

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,23 +1,37 @@
 document.addEventListener("DOMContentLoaded", function() {
     var planButton = document.querySelector("nav ul li button");
 
-    function showMenu(isOn) {
+    function showMenu(button, containerID, isOn) {
         if (isOn) {
-            document.getElementById("planMenu").style.display = "inline-block";
-            planButton.style.color = "navy";
-            planButton.style.backgroundColor = "white";
+            document.getElementById(containerID).style.display = "inline-block";
+            button.style.color = "navy";
+            button.style.backgroundColor = "white";
         } else {
-            document.getElementById("planMenu").style.display = "none";
-            planButton.style.color = "white";
-            planButton.style.backgroundColor = "navy";
+            document.getElementById(containerID).style.display = "none";
+            button.style.color = "white";
+            button.style.backgroundColor = "navy";
         }
     }
 
     planButton.addEventListener("click", function(event) {
         if (document.getElementById("planMenu").style.display == "inline-block") {
-            showMenu(false);
+            showMenu(planButton, "planMenu", false);
         } else {
-            showMenu(true);
+            showMenu(planButton, "planMenu", true);
         }
+    }, false)
+
+    var user = document.querySelector("#user");
+    user.addEventListener("click", function(event) {
+        if (document.getElementById("signIn").style.display == "inline-block") {
+            showMenu(user, "signIn", false);
+        } else {
+            showMenu(user, "signIn", true);
+        }
+    }, false);
+
+    document.getElementById("cancelButton").addEventListener("click", function(event) {
+        event.preventDefault();
+        showMenu(user, "signIn", false);
     }, false)
 })
