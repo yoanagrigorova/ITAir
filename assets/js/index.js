62aa4c91cb3d7688565a406882a695535c9dc5ec
yoanagrigorova <31156123+yoanagrigorova@users.noreply.github.com>
2017-09-22 11:39:32 +0300

    change of the position of js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,21 +1,23 @@
-var planButton = document.querySelector("nav ul li button");
+document.addEventListener("DOMContentLoaded", function() {
+    var planButton = document.querySelector("nav ul li button");
 
-function showMenu(isOn) {
-    if (isOn) {
-        document.getElementById("planMenu").style.display = "inline-block";
-        planButton.style.color = "navy";
-        planButton.style.backgroundColor = "white";
-    } else {
-        document.getElementById("planMenu").style.display = "none";
-        planButton.style.color = "white";
-        planButton.style.backgroundColor = "navy";
+    function showMenu(isOn) {
+        if (isOn) {
+            document.getElementById("planMenu").style.display = "inline-block";
+            planButton.style.color = "navy";
+            planButton.style.backgroundColor = "white";
+        } else {
+            document.getElementById("planMenu").style.display = "none";
+            planButton.style.color = "white";
+            planButton.style.backgroundColor = "navy";
+        }
     }
-}
 
-planButton.addEventListener("click", function(event) {
-    if (document.getElementById("planMenu").style.display == "inline-block") {
-        showMenu(false);
-    } else {
-        showMenu(true);
-    }
-}, false)
+    planButton.addEventListener("click", function(event) {
+        if (document.getElementById("planMenu").style.display == "inline-block") {
+            showMenu(false);
+        } else {
+            showMenu(true);
+        }
+    }, false)
+})
